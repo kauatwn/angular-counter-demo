fb7f6111e0ff78140ff7af30ae4a97529e084335
kauatwn <kaualopescontato@gmail.com>
2025-06-29 13:08:13 -0300

    Adiciona lógica de contagem com signal e componentes filhos

--- src/app/features/counter/counter.component.ts
+++ src/app/features/counter/counter.component.ts
@@ -1,10 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CounterControlsComponent } from './components/counter-controls/counter-controls.component';
+import { CounterDisplayComponent } from './components/counter-display/counter-display.component';
+import { CounterFooterComponent } from './components/counter-footer/counter-footer.component';
+import { CounterHeaderComponent } from './components/counter-header/counter-header.component';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [
+    CounterHeaderComponent,
+    CounterDisplayComponent,
+    CounterControlsComponent,
+    CounterFooterComponent,
+  ],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  count = signal(0);
+
+  increment() {
+    this.count.update((c) => c + 1);
+  }
+
+  decrement() {
+    this.count.update((c) => c - 1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+}
