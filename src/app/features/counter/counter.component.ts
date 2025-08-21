5e60f84c73a9c654afc7bcdf7efe75c0fab8c7ef
kauatwn <kaualopescontato@gmail.com>
2025-08-20 21:02:45 -0300

    Refatora CounterComponent para simplificar codebase

--- src/app/features/counter/counter.component.ts
+++ src/app/features/counter/counter.component.ts
@@ -1,16 +1,22 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { CounterActionsComponent } from './components/counter-actions/counter-actions.component';
-import { CounterDisplayComponent } from './components/counter-display/counter-display.component';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  signal,
+} from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMinus, faPlus, faSync } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-counter',
-  imports: [CounterDisplayComponent, CounterActionsComponent],
+  imports: [FontAwesomeModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent {
   protected readonly count = signal(0);
+  protected readonly shouldAnimate = signal(false);
 
   increment() {
     this.count.update((c) => c + 1);
@@ -23,4 +29,40 @@ export class CounterComponent {
   reset() {
     this.count.set(0);
   }
+
+  protected readonly buttons = [
+    {
+      icon: faMinus,
+      label: 'Diminuir',
+      action: () => this.decrement(),
+      className: 'btn btn-decrement',
+      ariaLabel: 'Diminuir contador',
+    },
+    {
+      icon: faSync,
+      label: 'Reiniciar',
+      action: () => this.reset(),
+      className: 'btn btn-reset',
+      ariaLabel: 'Reiniciar contador',
+    },
+    {
+      icon: faPlus,
+      label: 'Aumentar',
+      action: () => this.increment(),
+      className: 'btn btn-increment',
+      ariaLabel: 'Aumentar contador',
+    },
+  ];
+
+  private readonly setupAnimation = effect((onCleanup) => {
+    this.count();
+
+    this.shouldAnimate.set(true);
+
+    const timer = setTimeout(() => {
+      this.shouldAnimate.set(false);
+    }, 300);
+
+    onCleanup(() => clearTimeout(timer));
+  });
 }
